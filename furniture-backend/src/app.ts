fb8efe115297da0b6544b03afa253989f1e2a531
nainglinthant111 <oppositedirection111@gmail.com>
2025-04-04 13:20:59 +0630

    middleware in errorhandle

--- furniture-backend/src/app.ts
+++ furniture-backend/src/app.ts
@@ -1,14 +1,17 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import helmet from "helmet";
 import compression from "compression";
 import cors from "cors";
 import morgan from "morgan";
 
 import { limiter } from "./middlewares/rateLimiter";
+import { check } from "./middlewares/check";
 
 export const app = express();
+interface CustomRequeat extends Request {
+    userId?: number;
+}
 
-// helmet(), compression(), cors(), express.json()
 app.use(morgan("dev")) // morgan use log for req,res time
     .use(express.urlencoded({ extended: true })) // for request data
     .use(express.json()) //eg : Contact-Type:App;ication/json
@@ -18,8 +21,17 @@ app.use(morgan("dev")) // morgan use log for req,res time
     .use(limiter);
 
 // http://localhost:8080/health
-app.get("/health", (req, res) => {
+app.get("/health", check, (req: CustomRequeat, res: Response) => {
+    throw new Error("An error occurs!");
     res.status(200).json({
         message: "Request is Ok!",
+        userId: req.userId,
     });
+});
+
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+    const status = error.status || 500;
+    const message = error.message || "Server Error ";
+    const errorCode = error.code || "Error_Code";
+    res.status(status).json({ message: message, error: errorCode });
 });
---
+++ furniture-backend/src/middlewares/check.ts
@@ -0,0 +1,16 @@
+import { Request, Response, NextFunction } from "express";
+interface CustomRequeat extends Request {
+    userId?: number;
+}
+export const check = (
+    req: CustomRequeat,
+    res: Response,
+    next: NextFunction
+) => {
+    // const err: any = new Error("Token has Expired");
+    // err.status = 401;
+    // err.code = "Token_Expired";
+    // return next(err);
+    req.userId = 12345;
+    next();
+};
